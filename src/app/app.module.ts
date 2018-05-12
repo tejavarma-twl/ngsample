91fb5e21546f7f4820a5434318641edaa833a6c8
Teja Varma <tejavarma@Tejas-MacBook-Pro.local>
2018-05-12 13:14:50 +0530

    added components and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { AboutComponent } from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './services/services.component';
+import { Service1Component } from './services/service1/service1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductsComponent,
+    ServicesComponent,
+    Service1Component
   ],
   imports: [
     BrowserModule
