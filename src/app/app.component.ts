6286d3983200858800f4870a882189b7088b0571
Teja Varma <tejavarma@Tejas-MacBook-Pro.local>
2018-05-16 17:01:15 +0530

    dicussed bindings and directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   someName = 'Angular';
+  myColor = '#FFAA00';
   showthis = 0;
+  classSwitch = true;
+  inlineStyles = {
+                  'backgroundColor':'#00aa00',
+                  'color':'#99aa00',
+    'font-size':'22px'
+                };
+
   buttonClicked(value){
     this.someName = value;
   }
+
   addThisData(val){
     this.someName = val.target.value;
   }
 
+  bindingButtonClicked(){
+    if(this.myColor=='#FFAA00'){
+      this.myColor = '#00AAFF';
+    }else{
+      this.myColor = '#FFAA00';
+    }
+  }
+
+  switchingClass(){
+    if(this.classSwitch==true){
+      this.classSwitch = false;
+    }else{
+      this.classSwitch = true;
+    }
+  }
 }
