25d5c6b148fe7f6208b3bcd37ef4a087cc812de7
Teja Varma <tejavarma@Tejas-MacBook-Pro.local>
2018-05-19 12:40:37 +0530

    ng for discussed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,44 @@ export class AppComponent {
   myColor = '#FFAA00';
   showthis = 0;
   classSwitch = true;
+  about = true;
+  contact = false;
+  products = false;
+  services = false;
   inlineStyles = {
                   'backgroundColor':'#00aa00',
                   'color':'#99aa00',
     'font-size':'22px'
                 };
+
+  fruits = [
+    'apples',
+    'bananas',
+    'papayas',
+    'oranges'
+  ];
+  cars = [
+    {name:'BMW',color:'White'},
+    {name:'Audi',color:'Black'},
+    {name:'Merc',color:'Silver'},
+    {name:'Jaguar',color:'Blue'},
+    {name:'BMW',color:'White'},
+    {name:'Audi',color:'Black'},
+    {name:'Merc',color:'Silver'},
+    {name:'Jaguar',color:'Blue'}
+  ];
+
+  menuclick(page){
+    if(page === 'about'){
+      this.about = true; this.contact = false; this.products = false; this.services = false;
+    }else if(page === 'contact'){
+      this.contact = true; this.about = false; this.products = false; this.services = false;
+    }else if(page === 'products'){
+      this.products = true; this.contact = false; this.about = false; this.services = false;
+    }else {
+      this.services = true; this.contact = false; this.products = false; this.about = false;
+    }
+  }
 
   buttonClicked(value){
     this.someName = value;
